77ab6cd799746f9f12ed3c6ee5b931d879c04fdf
Vladislav Belov <vmbelov_3@edu.hse.ru>
2024-03-12 16:21:24 +0300

    separated mobile side

--- server/film.py
+++
@@ -1,7 +0,0 @@
-class Film:
-    def __init__(self, title, id, overview, poster_path):
-        self.title = title
-        self.api_id = id
-        self.overview = overview
-        self.poster_path = poster_path
-        self.review_average = 0
